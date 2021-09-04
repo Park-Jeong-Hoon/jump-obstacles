d14ef7ca6c96c51561013cc5888a3cf16e116ccb
Park-Jeong-Hoon <77821198+Park-Jeong-Hoon@users.noreply.github.com>
2021-09-04 10:44:57 +0900

    Add some function about collision between player and obstacle

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const game = document.getElementById("game");
 const player = document.getElementById("player");
+const obstacle = document.getElementById("obstacle");
 
 function jumping() {
+    //player의 classList에 jumping-animation이 없으면 추가해서 player가 점프를 하도록 한다.
     if(!player.classList.contains("jumping-animation")) {
         player.classList.add("jumping-animation");
     }
+
+    //setTimeout을 이용해서 player의 점프가 완료된 뒤 classList에서 jumping-animation 클래스를 제거
     setTimeout(function() {
         player.classList.remove("jumping-animation");
     }, 1000);
+};
+
+function collision() { //충돌할 경우
+    //player과 obstacle의 css 속성 중에 top과 left를 각각 가져와서
+    const playerTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"));
+    const obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"));
+    if(playerTop >= 400 && obstacleLeft > -50 && obstacleLeft < 50) { //충돌이 일어날 수 있는 위치값으로 조건문을 만든다.
+        obstacle.style.animation = "none";
+        obstacle.style.display = "none";
+        alert("game over");
+    }
 }
 
 game.addEventListener("click", jumping);
+
+setInterval(collision, 10);
