ea9611ae88f6f0a811fd9e94f9f2e7559912b3b8
Park-Jeong-Hoon <77821198+Park-Jeong-Hoon@users.noreply.github.com>
2021-09-06 10:22:06 +0900

    Make a css variable and add some codes to assign random color to this

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const game = document.getElementById("game");
 const player = document.getElementById("player");
 const obstacle = document.getElementById("obstacle");
 const score = document.querySelector("#score h1");
+const colors = ["red", "blue", "yellow", "pink", "green", "brown"];
 let sc = 0; //플레이어 점수
+
+obstacle.style.setProperty("--ob-color", colors[Math.floor(Math.random() * colors.length)]);
 
 function jumping() {
     //player의 classList에 jumping-animation이 없으면 추가해서 player가 점프를 하도록 한다.
@@ -16,16 +19,20 @@ function jumping() {
     }, 1000);
 };
 
-function collision() { //충돌할 경우
+function collision() { //충돌, 장애물 변경
     //player과 obstacle의 css 속성 중에 top과 left를 각각 가져와서
     const playerTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"));
     const obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"));
-    if(playerTop >= 400 && obstacleLeft > -50 && obstacleLeft < 50) { //충돌이 일어날 수 있는 위치값으로 조건문을 만든다.
+    if(playerTop >= 400 && obstacleLeft > -40 && obstacleLeft < 50) { //충돌이 일어날 수 있는 위치값으로 조건문을 만든다.(충돌)
         obstacle.style.animation = "none";
         obstacle.style.display = "none";
         //score.style.display = "none";
         clearInterval(interval); //setInterval을 멈춤
         alert(`your score: ${sc}, press F5 to restart game`);
+    }
+
+    if(obstacleLeft <= -40) { //장애물이 하나 지나가면 다른 색의 장애물이 나오도록 한다.(색변경)
+        obstacle.style.setProperty("--ob-color", colors[Math.floor(Math.random() * colors.length)]); //css의 속성 setProperty로 접근해서 값 변경
     }
 }
 
